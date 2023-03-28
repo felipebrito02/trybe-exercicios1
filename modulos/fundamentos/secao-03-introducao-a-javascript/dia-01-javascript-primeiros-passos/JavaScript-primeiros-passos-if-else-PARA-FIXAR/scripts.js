3c449909c4c727a332b24cfbfba78b4c6b8887a6
Felipe Brito <britofelipe149@gmail.com>
2023-03-28 15:21:50 -0300

    Exercício de fixação if-else

---
+++ modulos/fundamentos/secao-03-introducao-a-javascript/dia-01-javascript-primeiros-passos/JavaScript-primeiros-passos-if-else-PARA-FIXAR/scripts.js
@@ -0,0 +1,11 @@
+let nota = 85
+
+if (nota >= 80) {
+    console.log("Parabéns, você faz parte do nosso grupo de pessoas aprovadas!");
+}
+else if (nota <= 80 && nota >= 60) {
+    console.log("Você está na nossa lista de espera.");
+}
+else {
+    console.log("Infezlimente, você reprovou.");
+}
