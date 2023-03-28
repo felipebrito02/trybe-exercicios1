511fabebb6d0877bb77bc82662df3605dae321f0
Felipe Brito <britofelipe149@gmail.com>
2023-03-28 13:49:58 -0300

    Exercício de fixação

---
+++ modulos/fundamentos/secao-03-introducao-a-javascript/dia-01-javascript-primeiros-passos/JavaScript-primeiros-passos-tipos-primitivos-tp-dinamica-op-aritimetica-PARA-FIXAR/scripts.js
@@ -0,0 +1,9 @@
+let base = 5;
+let height = 8;
+let area = base * height;
+let perimeter = 2 * base + 2 * height;
+
+console.log(base)
+console.log(height)
+console.log(area)
+console.log(perimeter)
