40607d2b93c24fb976725af969b1aa8e1b72f483
Felipe Brito <britofelipe149@gmail.com>
2023-03-30 15:08:18 -0300

    Exercício de parametros pt2

--- modulos/fundamentos/secao-03-introducao-a-javascript/dia-03-javascript-funcoes/JavaScript-parametro-de-funcoes-PARA-PRATICAR/script.js
+++ modulos/fundamentos/secao-03-introducao-a-javascript/dia-03-javascript-funcoes/JavaScript-parametro-de-funcoes-PARA-PRATICAR/script.js
@@ -1,4 +1,4 @@
-let clientesTrybeBank = ['Ada', 'John', 'Gus'];
+/*let clientesTrybeBank = ['Ada', 'John', 'Gus'];
 
 function addClientes(nome) {
   if (typeof nome === "string") {
@@ -11,4 +11,30 @@ function addClientes(nome) {
 
 console.log(addClientes("Marcio "));
 console.log(addClientes (5));
-console.log(clientesTrybeBank);
+console.log(clientesTrybeBank); */
+
+let clientesTrybeBank = ['Ada', 'John', 'Gus'];
+
+function removeClientes(cliente) {
+  if (typeof cliente === "string") {
+    let clienteEncontrado = false;
+    for (let index = 0; index < clientesTrybeBank.length; index += 1) {
+      if (cliente === clientesTrybeBank[index]) {
+        return "Cliente removido do nosso sistema."
+      }
+      
+    }
+
+    if (clienteEncontrado === false) {
+      return "Cliente não encontrado"
+    }
+    
+  } else {
+    return "O parametro passado não é uma string"
+  }
+}
+
+console.log(removeClientes("Gus"));
+console.log(removeClientes("Fernanda"));
+console.log(removeClientes(10));
+
