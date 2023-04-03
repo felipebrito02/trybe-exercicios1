293d59709009fbdf331331376711bd6d2058d574
Felipe Brito <britofelipe149@gmail.com>
2023-04-03 18:48:41 -0300

    Exercícios objetos e for/in pt1

---
+++ modulos/fundamentos/secao-03-introducao-a-javascript/dia-04-javascript-objetos/JavaScript-objetos-exercicios-for-in/script.js
@@ -0,0 +1,37 @@
+let info = {
+  personagem: "Margarida",
+  origem: "Pato Donald",
+  nota: "Namorada do personagem principal nos quadrinhos do Pato Donald",
+};
+
+info.recorrente = "Sim"
+
+for (let keys in info) {
+  //console.log(keys);
+};
+ 
+
+//console.log(keys);
+//console.log(info);
+//console.log("Bem-vinda, " +info.personagem);
+
+for (let values in info) {
+  //console.log(info[values]);
+}
+
+let info2 = {
+  personagem: "Tio Patinhas",
+  origem: "Christmas on Bear Mountain, Dell’s Four Color Comics #178",
+  nota: "O último MacPatinhas",
+  recorrente: "Sim",
+};
+
+for (let key in info) {
+  if (
+    key === "recorrente" && info[key] === "Sim" && info2[key] === "Sim") 
+    {
+    console.log("Ambos recorrentes");
+  } else {
+    console.log(info[key] + " e " + info2[key]);
+  }
+}
