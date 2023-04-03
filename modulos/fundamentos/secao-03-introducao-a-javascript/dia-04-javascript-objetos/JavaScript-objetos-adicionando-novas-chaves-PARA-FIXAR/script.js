b6b730c4af0ceaeb649a374d1b53d97e77d92e2d
Felipe Brito <britofelipe149@gmail.com>
2023-04-03 15:39:36 -0300

    Exercício de fixação de adição de novas chaves

---
+++ modulos/fundamentos/secao-03-introducao-a-javascript/dia-04-javascript-objetos/JavaScript-objetos-adicionando-novas-chaves-PARA-FIXAR/script.js
@@ -0,0 +1,13 @@
+let student = {};
+
+  function addProperty(object, key, value) {
+    object[key] = value;
+  }
+
+addProperty(student, "nome", "Felipe Brito")
+addProperty(student, "email", "britofelipe149@gmail.com")
+addProperty(student, "github", "https://github.com/felipebrito02")
+addProperty(student, "linkedin", "https://www.linkedin.com/in/felipebrito02/")
+
+
+
