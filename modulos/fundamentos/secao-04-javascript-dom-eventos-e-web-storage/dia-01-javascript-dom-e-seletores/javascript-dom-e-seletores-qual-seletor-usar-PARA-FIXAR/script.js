baa3c2264d54a50e6e29e817a1c98c04dc38aff5
Felipe Brito <britofelipe149@gmail.com>
2023-04-10 19:02:39 -0300

    Fix

--- modulos/fundamentos/secao-04-javascript-dom-eventos-e-web-storage/dia-01-javascript-dom-e-seletores/javascript-dom-e-seletores-qual-seletor-usar-PARA-FIXAR/script.js
+++ modulos/fundamentos/secao-04-javascript-dom-eventos-e-web-storage/dia-01-javascript-dom-e-seletores/javascript-dom-e-seletores-qual-seletor-usar-PARA-FIXAR/script.js
@@ -6,16 +6,16 @@ urgenteImportante[0].style.backgroundColor = 'salmon';
 
 const styleUrgenteImportante = document.querySelectorAll('.emergency-tasks h3');
 for (let i = 0; i < styleUrgenteImportante.length; i += 1) {
-  styleUrgenteImportante[i].style.backgroundColor = 'hotpink'
+  styleUrgenteImportante[i].style.backgroundColor = 'hotpink';
 }
 
 const naoUrgenteImportante = document.getElementsByClassName('no-emergency-tasks');
-naoUrgenteImportante[0].style.backgroundColor = 'yellow'
+naoUrgenteImportante[0].style.backgroundColor = 'yellow';
 
 const styleNaoUrgenteImpornte = document.querySelectorAll('.no-emergency-tasks h3');
-for (let i = 0; i < naoUrgenteImportante.length; i += 1) {
-  styleNaoUrgenteImpornte[i].style.backgroundColor = 'black'
+for (let i = 0; i < styleNaoUrgenteImpornte.length; i += 1) {
+  styleNaoUrgenteImpornte[i].style.backgroundColor = 'black';
 }
 
 const rodape = document.getElementById('footer-container');
-rodape.style.backgroundColor = 'darkgreen'
+rodape.style.backgroundColor = 'darkgreen';
