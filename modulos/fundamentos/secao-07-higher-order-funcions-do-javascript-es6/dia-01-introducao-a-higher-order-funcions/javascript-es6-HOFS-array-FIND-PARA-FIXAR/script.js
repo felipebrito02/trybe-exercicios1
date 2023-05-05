908b62fe3576cb03c84c1d2c00ab78ea5abd3048
Felipe Brito <britofelipe149@gmail.com>
2023-05-05 15:36:49 -0300

    Exercicios de fixação .find

---
+++ modulos/fundamentos/secao-07-higher-order-funcions-do-javascript-es6/dia-01-introducao-a-higher-order-funcions/javascript-es6-HOFS-array-FIND-PARA-FIXAR/script.js
@@ -0,0 +1,7 @@
+const numbers = [19, 21, 30, 3, 45, 22, 15];
+
+// Adicione seu código aqui
+
+const verificarnumeros =numbers.find((numbers) => numbers % 3 === 0 && numbers % 5 === 0 );
+
+console.log(verificarnumeros);
---
+++ modulos/fundamentos/secao-07-higher-order-funcions-do-javascript-es6/dia-01-introducao-a-higher-order-funcions/javascript-es6-HOFS-array-FIND-PARA-FIXAR/script2.js
@@ -0,0 +1,7 @@
+const names = ['João', 'Irene', 'Fernando', 'Maria'];
+
+// Adicione seu código aqui
+
+const verificaNome = names.find((names) => names.length === 5);
+
+console.log(verificaNome);
